0940566a15d0bf87fa2150344567d6bce62ccf54
vitor-ufms <vitor-ufms>
2023-11-08 10:12:38 -0400

    docker ok, trigger na pasta resources

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/concessionaria', {
-    host: 'localhost',
+const sequelize = new Sequelize('postgres://postgres:postgres@175.20.0.2:5432/concessionaria', {
+    host: '175.20.0.2',
     dialect: 'postgres'
 }) // user:senha@host:port/database
 
